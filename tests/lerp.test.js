85aa2e3129bf7fb2867fe559ed09495c51c45bf7
aguilana <naguila.mandich@gmail.com>
2023-05-02 18:37:48 -0400

    reset some commits, added jest, working fine

---
+++ tests/lerp.test.js
@@ -0,0 +1,17 @@
+const lerp = require('../utils/lerp');
+
+describe('lerp function', () => {
+    it('should return the start value when percent is 0', () => {
+        expect(lerp(0, 10, 0)).toBe(0);
+    });
+
+    it('should return the end value when percent is 1', () => {
+        expect(lerp(0, 10, 1)).toBe(10);
+    });
+
+    it('should return the correct value for a percentage between 0 and 1', () => {
+        expect(lerp(0, 10, 0.5)).toBe(5);
+        expect(lerp(0, 100, 0.25)).toBe(25);
+        expect(lerp(-50, 50, 0.75)).toBe(25);
+    });
+});
--- utils/lerp.js
+++ utils/lerp.js
@@ -3,3 +3,5 @@ function lerp(start, end, percent) {
     // 0 and 1 just gives the 2 points
     return start + (end - start) * percent; // when percent is 0, return start. when percent is 1, return end
 }
+
+module.exports = lerp; // export the lerp function
